805903795bf6f8bd4d66917bb6e34ba27d579151
Rafael Galindo <rgf1996cbta3@gmail.com>
2025-06-18 15:35:27 -0600

    cambio2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Pérez Trujillo Víctor Hugo
+          Galindo Flores Rafael
         </p>
         <a
           className="App-link"
